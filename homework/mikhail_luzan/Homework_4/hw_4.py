2b8c69adc0d68c9de3dd51e1571f64061f01c7a1
Mikhail Luzan <mikhail.luzan.2021@gmail.com>
2024-03-15 10:57:34 +0300

    homework_4 done

---
+++ homework/mikhail_luzan/Homework_4/hw_4.py
@@ -0,0 +1,18 @@
+my_dict = {}
+my_dict['tuple'] = (5, 9, 'text', True, 3)
+my_dict['list'] = ['first', 159, None, 753, 'last']
+my_dict['dict'] = {'first': '1234', 'second': '987654', 'third': '1', 'fourth': '25836', 'fifth': '458'}
+my_dict['set'] = {'text', 34, False, 55.55, True}
+
+print("The last element of 'tuple' key:", my_dict['tuple'][-1])
+
+my_dict['list'].append(1544)
+my_dict['list'].pop(1)
+
+my_dict['dict']['i am a tuple'] = (1, 2, 3)
+my_dict['dict'].pop('fifth')
+
+my_dict['set'].add('new')
+my_dict['set'].remove(False)
+
+print("Updated dictionary:", my_dict)
